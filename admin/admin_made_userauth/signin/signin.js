55b6773b19d60e1b3276ede1fbfd41f44d3969b2
Rajrup-TransEV <transmogrify17@outlook.com>
2024-07-25 16:40:15 +0530

    user signin logic hasbeen written and some secure changes added some bug fix added

---
+++ admin/admin_made_userauth/signin/signin.js
@@ -0,0 +1,59 @@
+//admin made user login routes
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+const prisma = new PrismaClient()
+
+const adminuserlogin = async (req, res) => {
+    const apiauthkey = req.headers['apiauthkey'];
+    // Check if the API key is valid
+    if (!apiauthkey || apiauthkey !== process.env.API_KEY) {
+        return res.status(403).json({ message: "API route access forbidden" });
+    }
+
+    const { phone, email, password } = req.body;
+
+    try {
+        const existingUser = await prisma.userProfile.findFirst({
+            where: {
+                OR: [
+                    { email: email },
+                    { phonenumber: phone }
+                ]
+            },
+            select: {
+                email: true,
+                phonenumber: true,
+                uid: true,
+                password: true,
+                role: true
+            }
+        });
+
+        if (!existingUser) {
+            return res.status(404).json({ message: "Wrong credentials" });
+        }
+
+        const checkPassword = await bcrypt.compare(password, existingUser.password);
+
+        if (!checkPassword) {
+            return res.status(404).json({ message: "Password does not match. Failed to login" });
+        }
+
+        const token = jwt.sign(
+            {
+                email: existingUser.email,
+                userid: existingUser.uid,
+                userType: existingUser.userType,
+            },
+            process.env.JWT_SECRET, // Make sure to set your JWT secret in environment variables
+            { expiresIn: '1h' } // Token expiration time
+        );
+
+        return res.status(201).json({ message: "Login success", authtoken: token });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+};
+export  default adminuserlogin
--- admin/admin_made_userroutes/admin_made_user_routes.js
+++ admin/admin_made_userroutes/admin_made_user_routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express"
 // import {admincreateuser} from "
 import admincreateuser from "../admin_to_user_profile/admin_to_user_profile_create.js"
+import adminuserlogin from "../admin_made_userauth/signin/signin.js"
 const adminmadeuserroutes = Router()
 
 adminmadeuserroutes.post("/create/userprofilecreate",admincreateuser)
+adminmadeuserroutes.post("/login/userlogin",adminuserlogin)
 
 export default adminmadeuserroutes;
