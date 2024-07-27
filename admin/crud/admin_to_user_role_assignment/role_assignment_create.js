8d3467070aa0403fe9abf16f6b4476d3f3dfcc01
Rajrup-TransEV <transmogrify17@outlook.com>
2024-07-27 09:59:59 +0530

    added role assigment model to the user

---
+++ admin/crud/admin_to_user_role_assignment/role_assignment_create.js
@@ -0,0 +1,32 @@
+//role assign to user create
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+const createrole = async(req,res)=>{
+try {
+        
+    
+  const {userid,rolename,  roledesc} = req.body
+  if (!userid){
+    return res.status(400).json("user id is required to assign the role")
+  }
+
+  const process =  await prisma.assignRoles.create({
+        data:{
+            uid:crypto.randomUUID(),
+            userid:userid,
+            rolename:rolename,
+            roledesc:roledesc
+        }
+})
+if (!process){
+    return res.status(503).json("Cannot create the role due to some issue at backend")
+}
+return res.status(200).json("role for the perticualr habeen created successfully")
+} catch (error) {
+ console.log(error)   
+ return res.status(500).json({message:`message processing error occurred ${error}`})    
+}
+}
+
+export default createrole
---
+++ prisma/migrations/20240727035707_additional_field_added_to_assign_roles/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `AssignRoles` ADD COLUMN `userid` VARCHAR(191) NULL;
