4a8d8778a5123a552be13b04530aeac13a1d2f00
Rajrup-TransEV <transmogrify17@outlook.com>
2024-08-09 17:11:05 +0530

    config added

--- admin/crud/financial_details/get_all_financial_details.js
+++ admin/crud/financial_details/get_all_financial_details.js
@@ -1,6 +1,6 @@
 //get all of the stored financial details 
 import { PrismaClient } from "@prisma/client";
-
+import logging from "../../../logging/logging_generate"
 const prisma = new PrismaClient();
 
 const getallfinancialdetails  =  async(req,res)=>{
@@ -8,6 +8,10 @@ const getallfinancialdetails  =  async(req,res)=>{
     
     // Check if the API key is valid
     if (!apiauthkey || apiauthkey !== process.env.API_KEY) {
+        const messagetype = "error"
+        const message = "API route access error"
+        const filelocation = "get_all_financial_details.js"
+        logging(messagetype,message,filelocation)
         return res.status(403).json({ message: "API route access forbidden" });
     }
     try {
@@ -15,10 +19,18 @@ const getallfinancialdetails  =  async(req,res)=>{
             if(!allfinancialaccountdetails){
                 return res.status(404).json("no financial details found please enter some details in db first")
             }
-
-            return res.status(200).json({data:allfinancialaccountdetails})
+            const messagetype = "success"
+            const message = "API route access error"
+            const filelocation = "get_all_financial_details.js"
+            logging(messagetype,message,filelocation)
+            return res.status(200).json({message:"all the data are listed",data:allfinancialaccountdetails})
     } catch (error) {
         console.log(error)
+        const messagetype = "success"
+        const message = `Error while showing the data ${error}`
+        const filelocation = "get_all_financial_details.js"
+        logging(messagetype,message,filelocation)
+        return res.status(500).json({message:"Error while showing the data",data:`${error}`})
     }
 
 }
