1aca9df62c8e33ccc15ee62e41e753ec42844002
Rajrup-TransEV <transmogrify17@outlook.com>
2024-08-17 09:39:12 +0530

    some changes added to qrcode generator

--- androidpac/controller/crud/qrcodeops/qrcode.js
+++ androidpac/controller/crud/qrcodeops/qrcode.js
@@ -1,9 +1,11 @@
+//qr code generator
 import qrcode from "qrcode"
 
 
 
 const generateqrcode  = async (req,res)=>{
     try {
+        
         const data = "myname is"
         const qrcodeurl = await qrcode.toDataURL(data)
         return res.status(200).json({qrcodeurl})
