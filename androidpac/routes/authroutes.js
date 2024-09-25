fbd62aaa06398954d22593791fa7f0e2399a599f
Rajrup-TransEV <transmogrify17@outlook.com>
2024-09-25 12:05:37 +0530

    regenrate otp route given

--- androidpac/routes/authroutes.js
+++ androidpac/routes/authroutes.js
@@ -4,10 +4,12 @@ import { loginUser } from "../auth/login/loginuser.js";
 import { verifyuser } from "../auth/authverify.js";
 import verifyOTP from "../auth/verifyOTP.js";
 import verifyloginOTP from "../auth/login/verifylogin.js";
+import regenerateOtp from "../../lib/regenerateOtp.js";
 const authRoutes = Router()
 authRoutes.post("/signup",signupUser)
 authRoutes.post("/login",loginUser)
 authRoutes.post("/verifyuser",verifyuser)
 authRoutes.post("/verifyotp",verifyOTP)
 authRoutes.post("/verifyloginotp",verifyloginOTP)
+authRoutes.post("/regenrateotp",regenerateOtp)
 export default authRoutes;
